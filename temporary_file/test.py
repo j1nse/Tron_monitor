20dbad045f2a3c918a8aba5dbb1b994ca38e9a31
shequ123 <lgagl@qq.com>
2018-11-27 18:41:12 +0800

    update ignore

--- temporary_file/test.py
+++
@@ -1,8 +0,0 @@
-#coding:utf-8
-from tronapi import HttpProvider, Tron
-full_node = HttpProvider('https://api.trongrid.io')
-solidity_node = HttpProvider('https://api.trongrid.io')
-event_server = 'https://api.trongrid.io'
-tron = Tron()
-tron.default_block = 'latest'
-tron.get_block('latest')
