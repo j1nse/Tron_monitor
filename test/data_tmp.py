ffed1279422c37380169d4e74ce261c3965dfef1
shequ123 <lgagl@qq.com>
2018-12-18 14:44:18 +0800

    downdate

---
+++ test/data_tmp.py
@@ -0,0 +1,75 @@
+a = {'transaction': [{'ret': [{'contractRet': 'SUCCESS'}], 'signature': [
+    '0f25c7d6f93392ccbfe8a81b67a5913d3ee9735200cc6c6d66770604082cac2c5ce7e15190475056bcd630d6d096f3c13ef5aa7408a81b05c7ae3564673cb24001'],
+                      'txID': '9e163ed4a6d43ad840f62223729eef35b187bd0240e6767255c425ac158c9fc2', 'raw_data': {
+        'contract': [{'parameter': {'value': {'amount': 1834, 'asset_name': '53454544',
+                                              'owner_address': '41a94763c92d7e6b79dd87d99ce622e225237c450b',
+                                              'to_address': '41c5a162189b779237d481e542d56f0c3dc0995ce4'},
+                                    'type_url': 'type.googleapis.com/protocol.TransferAssetContract'},
+                      'type': 'TransferAssetContract'}], 'ref_block_bytes': 'd3bd',
+        'ref_block_hash': '458b984b0d8883ff', 'expiration': 1544491458000, 'timestamp': 1544491160834}},
+                     {'ret': [{'contractRet': 'SUCCESS'}], 'signature': [
+                         '990073ffd7b08bfa204bf33e5c0594756ada7495b80452ceedfb83e72fe98b051b00396561a75b1fd51ac9e1e41ea5266dafff882b6972921a2375fc10317fd401'],
+                      'txID': '041b67ebfccce58ac650446e9093aecc3e05764cbbcacb938ffaa525de956a0f', 'raw_data': {
+                         'contract': [{'parameter': {'value': {'amount': 1485302129,
+                                                               'owner_address': '41beab998551416b02f6721129bb01b51fceceba08',
+                                                               'to_address': '41c5a162189b779237d481e542d56f0c3dc0995ce4'},
+                                                     'type_url': 'type.googleapis.com/protocol.TransferContract'},
+                                       'type': 'TransferContract'}], 'ref_block_bytes': 'e2d1',
+                         'ref_block_hash': '178d676c5d7c6c37', 'expiration': 1544503155000,
+                         'timestamp': 1544502862312}}, {'ret': [{'contractRet': 'SUCCESS'}], 'signature': [
+        '8a3bb7958b5d88850607af121e1f61660d187048a73b004661103cb5831aac614c7dd5b3c2a25d9f14998fb1239d588071191a6e77ea5f87a0c10544d88a4fd901'],
+                                                        'txID': '955b9ca0be168a0ac643529ef5e8d14abbff616d862e2148067ed1ecc6ac147a',
+                                                        'raw_data': {'contract': [{'parameter': {
+                                                            'value': {'amount': 13, 'asset_name': '54524f4e4f4e45',
+                                                                      'owner_address': '41f70386347e689e6308e4172ed7319c49c0f66e0b',
+                                                                      'to_address': '41c5a162189b779237d481e542d56f0c3dc0995ce4'},
+                                                            'type_url': 'type.googleapis.com/protocol.TransferAssetContract'},
+                                                            'type': 'TransferAssetContract'}],
+                                                            'ref_block_bytes': 'e949',
+                                                            'ref_block_hash': '3367fc7de3176880',
+                                                            'expiration': 1544507994000,
+                                                            'timestamp': 1544507937941}},
+                     {'ret': [{'contractRet': 'SUCCESS'}], 'signature': [
+                         '21c5591de40a7e88dc59b4d748d5f21b2d6ecf4590ca50592d1f90c681f0906c56d5f5fd7d815e80948676b3a18bbcb0ffca1acde99b2cb6db98b25cf586d0ba01'],
+                      'txID': '4a47eb9cdb1a1dd3c34f71b74c13cd40dcbad05c59e8cfc23e3680e79e8d637b', 'raw_data': {
+                         'contract': [{'parameter': {'value': {'amount': 2410, 'asset_name': '53454544',
+                                                               'owner_address': '41a94763c92d7e6b79dd87d99ce622e225237c450b',
+                                                               'to_address': '41c5a162189b779237d481e542d56f0c3dc0995ce4'},
+                                                     'type_url': 'type.googleapis.com/protocol.TransferAssetContract'},
+                                       'type': 'TransferAssetContract'}], 'ref_block_bytes': '3dfb',
+                         'ref_block_hash': 'd5598fcfd0decbd6', 'expiration': 1544573985000,
+                         'timestamp': 1544573688387}}, {'ret': [{'contractRet': 'SUCCESS'}], 'signature': [
+        'c4cd9dcc0147edc0e2f2aa49597346eafc3c2b2b9bc3402427d2c0c983419df2359eccefa184162ff2f1a178408f1c5ed698904c31771777830f5a9c12bc6f7d00'],
+                                                        'txID': '5c86a857314f7918a198d3f8472e87d8f2e021bb6131712426ab8b72b34f9f42',
+                                                        'raw_data': {'contract': [{'parameter': {
+                                                            'value': {'amount': 2390, 'asset_name': '53454544',
+                                                                      'owner_address': '41a94763c92d7e6b79dd87d99ce622e225237c450b',
+                                                                      'to_address': '41c5a162189b779237d481e542d56f0c3dc0995ce4'},
+                                                            'type_url': 'type.googleapis.com/protocol.TransferAssetContract'},
+                                                            'type': 'TransferAssetContract'}],
+                                                            'ref_block_bytes': 'ab1d',
+                                                            'ref_block_hash': '849166b08a692917',
+                                                            'expiration': 1544660478000,
+                                                            'timestamp': 1544660181575}},
+                     {'ret': [{'contractRet': 'SUCCESS'}], 'signature': [
+                         '9d0ecc5177c6e7c6aeec0cc01f746a4f90b127f2f84df3c757cfefd76711a79c230ed52b74bcc2873cb9d20a9b6c322bc0bf868f689fc67f8937147d95431d6400'],
+                      'txID': '5a23450f445df176ea71ec897fe4b66ef32580b19b0864c0a1c339bb6136c5a8', 'raw_data': {
+                         'contract': [{'parameter': {'value': {'amount': 2379, 'asset_name': '53454544',
+                                                               'owner_address': '41a94763c92d7e6b79dd87d99ce622e225237c450b',
+                                                               'to_address': '41c5a162189b779237d481e542d56f0c3dc0995ce4'},
+                                                     'type_url': 'type.googleapis.com/protocol.TransferAssetContract'},
+                                       'type': 'TransferAssetContract'}], 'ref_block_bytes': '1877',
+                         'ref_block_hash': '99e70e6b6309c09a', 'expiration': 1544747574000,
+                         'timestamp': 1544747276999}}, {'ret': [{'contractRet': 'SUCCESS'}], 'signature': [
+        '4b39419a2082e92d4bf04f4b62bb754bcdc369cb27e87958467c3cddc3f59e2b516e97011d46d8f2ea24e55123ccf32190106369ae8a71bc514bad718c3a821e01'],
+                                                        'txID': 'd14e97e6505bcf73003448416e912d79e4d1d0b139c2cf21656c567cc6bf1ac6',
+                                                        'raw_data': {'contract': [{'parameter': {
+                                                            'value': {'amount': 2356, 'asset_name': '53454544',
+                                                                      'owner_address': '41a94763c92d7e6b79dd87d99ce622e225237c450b',
+                                                                      'to_address': '41c5a162189b779237d481e542d56f0c3dc0995ce4'},
+                                                            'type_url': 'type.googleapis.com/protocol.TransferAssetContract'},
+                                                            'type': 'TransferAssetContract'}],
+                                                            'ref_block_bytes': '831c',
+                                                            'ref_block_hash': '4b34601f6eb339f0',
+                                                            'expiration': 1544834151000,
+                                                            'timestamp': 1544833854177}}]}
--- test/test_api.py
+++ test/test_api.py
@@ -7,16 +7,16 @@ proxies = {
     'https': 'socks5://127.0.0.1:1080'
     }
 }
-full_node = HttpProvider('https://api.trongrid.io',)
-solidity_node = HttpProvider('https://api.trongrid.io',)
-event_server = HttpProvider('https://api.trongrid.io',)
+full_node = HttpProvider('https://api.trongrid.io',proxies)
+solidity_node = HttpProvider('https://api.trongrid.io',proxies)
+event_server = HttpProvider('https://api.trongrid.io',proxies)
 
 
 tron = Tron(full_node=full_node,
 		solidity_node=solidity_node,
 		event_server=event_server)
-tron.default_block = 'latest'
 api=trx.Trx(tron)
-#print(api.get_current_block())
-print(api.get_transaction('c26a332a2a10f03bef44002d28484ae626e105d63f5eaad26dbda33d32ec76d6'))
+# print(api.get_current_block())
+# TBv7igM3eVRsd144ayw7X3ecxsAdMQfBne
+print(api.get_transactions_related('TTzBSx4J2j6ZYF6YtDSGDAR7k7k7t7jMPF','to',10,0))
 
